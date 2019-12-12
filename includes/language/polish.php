77122703108ecdccb260e5d53d995540fdf58749
Damian Zamojski <damian.zamojski1@gmail.com>
2019-12-12 15:59:57 +0100

    Added customer's polish language.

---
+++ includes/language/polish.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * @package HelpDeskZ
+ * @website: http://www.helpdeskz.com
+ * @community: http://community.helpdeskz.com
+ * @author br33f
+ * @since 1.0.2
+ */
+$LANG['HOME'] = 'Strona główna';
+$LANG['MY_TICKETS'] = 'Moje zgłoszenia';
+$LANG['SUBMIT_A_TICKET'] = 'Wyślij zgłoszenie';
+$LANG['KNOWLEDGEBASE'] = 'Baza wiedzy';
+$LANG['NEWS'] = 'Aktualności';
+$LANG['ACCOUNT_LOGIN'] = 'Logowanie';
+$LANG['LASTEST_UPDATES'] = 'Ostatnie aktualizacje';
+$LANG['YOUR_EMAIL_ADDRESS'] = 'Twój adres e-mail';
+$LANG['YOUR_PASSWORD'] = 'Twoje hasło';
+$LANG['REMEMBER_ME'] = 'Zapamiętaj mnie';
+$LANG['LOST_PASSWORD'] = 'Zapomniane hasło';
+$LANG['LOGIN'] = 'Login';
+$LANG['ACCOUNT'] = 'Konto';
+$LANG['MY_PROFILE'] = 'Mój profil';
+$LANG['PREFERENCES'] = 'Ustawienia';
+$LANG['CHANGE_PASSWORD'] = 'Zmień hasło';
+$LANG['LOGOUT'] = 'Wyloguj';
+$LANG['VIEW_ALL_NEWS'] = 'Zobacz wszystkie aktualności';
+$LANG['POSTED_BY_ON'] = 'Opublikowane przez %author% dnia %date%';
+$LANG['OLDEST_POSTS'] = 'Najstarsze wpisy';
+$LANG['NEWEST_POSTS'] = 'Najnowsze wpisy';
+$LANG['MOST_POPULAR_ARTICLES'] = 'Najpopularniejsze artykuły';
+$LANG['NEWEST_ARTICLES'] = 'Najnowsze artykuły';
+$LANG['MORE_TOPICS'] = 'Więcej tematów';
+$LANG['FILE_NOT_FOUND'] = 'Plik nieodnaleziony.';
+$LANG['SEARCH'] = 'Wyszukaj';
+$LANG['SEARCH_IN_KNOWLEDGEBASE'] = 'Wyszukaj w bazie wiedzy';
+$LANG['SEARCH_RESULTS'] = 'Wyniki wyszukiwania';
+$LANG['SEARCH_NOT_FOUND'] = 'Nie odnaleziono pasujących wyników';
+$LANG['ENTER_EMAIL_RECOVER_PASSWORD'] = 'Wpisz adres e-mail, na który jest zarejestrowane konto i naciśnij przycisk Zatwierdź. Wkrótce otrzymasz wiadmość e-mail z odnośnikiem do zresetowania swojego hasła.';
+$LANG['ACCOUNT_INFORMATION'] = 'Informacje o koncie';
+$LANG['EMAIL'] = 'E-mail';
+$LANG['CAPTCHA_VERIFICATION'] = 'Weryfikacja CAPTCHA';
+$LANG['ENTER_TEXT_CAPTCHA'] = 'Wpisz tekst, który widzisz na obrazku w poniższe pole (aby zapobiegać wysyłaniu zgłoszeń przez roboty).';
+$LANG['SUBMIT'] = 'Zatwierdź';
+$LANG['CANT_FIND_SOLUTION_SUBMIT_TICKET'] = 'Jeżeli nie odnalazłeś rozwiązania swojego problemu, możesz wysłać zgłoszenie wybierając odpowiedni dział poniżej.';
+$LANG['SELECT_DEPARTMENT_TICKET'] = 'Wybierz dział zgłoszenia';
+$LANG['CSRF_ERROR'] = 'Napotkaliśmy problem (wykryto CSRF); Spróbuj ponownie.';
+$LANG['DEPARTMENTS'] = 'Działy';
+$LANG['NEXT'] = 'Dalej';
+$LANG['INVALID_EMAIL_OR_PASSWORD'] = 'Nieprawidłowy adres e-mail lub hasło.';
+$LANG['YOUR_TICKET_DETAILS'] = 'Szczegóły Twojego zgłoszenia';
+$LANG['ENTER_TICKET_DETAILS'] = 'Wpisz dane Twojego zgloszenia poniżej. Jeżeli zgłaszany jest błąd, pamiętaj proszę o przesłaniu jak największej ilości istotnych informacji.';
+$LANG['GENERAL_INFORMATION'] = 'Ogólne informacje';
+$LANG['FULL_NAME'] = 'Pełna nazwa';
+$LANG['PRIORITY'] = 'Priorytet';
+$LANG['YOUR_MESSAGE'] = 'Treść wiadmości';
+$LANG['SUBJECT'] = 'Temat';
+$LANG['ATTACHMENTS'] = 'Załączniki';
+$LANG['INVALID_CAPTCHA_CODE'] = 'Błąd: Nieprawidłowy kod weryfikacji';
+$LANG['INVALID_EMAIL_ADDRESS'] = 'Błąd: Wpisz prawidłowy adres e-mail';
+$LANG['ONE_REQUIRED_FIELD_EMPTY'] = 'Jedno z wymaganych pól jest puste.';
+$LANG['ERROR_UPLOADING_A_FILE'] = 'Wystąpił błąd poczas wgrywania pliku.';
+$LANG['INVALID_FILE_EXTENSION'] = 'Nieprawidłowy format pliku.';
+$LANG['FILE_NOT_ALLOWED'] = 'Plik jest niedozwolony.';
+$LANG['FILE_IS_BIG'] = 'Plik jest zbyt duży. Maksymalny rozmiar pliku to %size%';
+$LANG['REQUEST_RECEIVED'] = 'Twoje zgłoszenia zostało odebrane';
+$LANG['REQUEST_RECEIVED_RESPONSE'] = 'Otrzymaliśmy Twoje zgłoszenie, nasz zespół odpowie na nie wkrótce. Możesz się zalogować do centrum pomocy, aby sprawdzić status swojego zgłoszenia. O aktualizacji stanu zgłoszenia będziesz informowany także przez wiadomości e-mail.';
+$LANG['TICKET_ID'] = 'ID zgłoszenia';
+
+$LANG['WE_SENT_YOUR_NEW_PASSWORD'] = 'Na podany adres wysłano wiadomość e-mail z nowym hasłem.';
+$LANG['EMAIL_WAS_NOT_FOUND'] = 'Adres e-mail nie odnaleziony.';
+$LANG['SEARCH_TICKET_ID'] = 'Wyszukaj ID zgłoszenia';
+$LANG['VIEW_TICKETS'] = 'Zobacz zgłoszenia';
+$LANG['VIEW_TICKETS_DESCRIPTION'] = 'Poniżej znajduje się lista zgłoszeń, które wcześniej wysłałeś. Kliknij na temat zgłoszenia, aby zobaczyć jego szczegóły i historie.';
+$LANG['LAST_UPDATE'] = 'Ostatnie zmiany';
+$LANG['DEPARTMENT'] = 'Dział';
+$LANG['STATUS'] = 'Status';
+$LANG['VIEW_TICKET_ID'] = 'Zobacz zgłoszenie #%id%';
+$LANG['CREATED'] = 'Utworzono';
+$LANG['UPDATED'] = 'Zaktualizowano';
+$LANG['TICKET_IS_CLOSED'] = 'Zgłoszenie jest obecnie zamknięte.';
+$LANG['ADD_REPLY'] = 'Odpowiedz';
+$LANG['YOUR_REPLY_SENT'] = 'Twoja odpowiedź do tego zgłoszenia została pomyślnie wysłana';
+$LANG['USER'] = 'Użytkownik';
+$LANG['STAFF'] = 'Personel';
+$LANG['POSTED_ON'] = 'Wpis z dnia';
+$LANG['TICKET_NOT_FOUND_OR_PERMISSION'] = 'Przepraszamy, nie odnaleziono zgłoszenia lub nie masz odpowiednich uprawnień, aby je zobaczyć.';
+$LANG['TICKET_NOT_FOUND'] = 'Zgłoszenie nie odnalezione.';
+$LANG['YOUR_PROFILE_UPDATED'] = 'Twój profil został zaktualizowany.';
+$LANG['UPDATE'] = 'Aktualizacja';
+$LANG['YOUR_PREFERENCES_SAVED'] = 'Twoje ustawienia zostały zapisane.';
+$LANG['GENERAL_OPTIONS'] = 'Ogólne opcje';
+$LANG['TIMEZONE'] = 'Strefa czasowa';
+$LANG['DEFAULT_TIMEZONE'] = 'Domyślna strefa czasowa';
+$LANG['PASSWORD_UPDATED'] = 'Hasło zostało zaktualizowane.';
+$LANG['PASSWORD_DETAILS'] = 'Szczególy dot. hasła';
+$LANG['EXISTING_PASSWORD'] = 'Obecne hasło';
+$LANG['NEW_PASSWORD'] = 'Nowe hasło';
+$LANG['CONFIRM'] = 'Potwierdź';
+$LANG['EXISTING_PASSWORD_INCORRECT'] = 'Obecne hasło jest nieprawidłowe.';
+$LANG['NEW_PASSWORDS_DO_NOT_MATCH'] = 'Wpisane nowe hasła różnią się.';
+$LANG['EMAIL_ASSOCIATED_OTHER_ACCOUNT'] = 'Ten adres e-mail jest już powiązany z istniejącym kontem.';
+$LANG['WE_ARE_PERFORMING_MAINTENANCE'] = 'Obecnie trwa planowana przerwa techniczna. Przepraszamy za utrudnienia.';
+$LANG['MAINTENANCE_MODE'] = 'Przerwa techniczna';
+$LANG['PAGE_NOT_FOUND'] = 'Nie odnaleziono strony- 404';
+$LANG['CLICK_TO_BACK_HELPDESK'] = 'Kliknij, aby przejść do strony głównej centrum pomocy';
+$LANG['OPEN'] = 'Otwórz';
+$LANG['AWAITING_REPLY'] = 'Oczekuje na odpowiedź';
+$LANG['IN_PROGRESS'] = 'W trakcie';
+$LANG['ANSWERED'] = 'Odpowiedziano';
+$LANG['CLOSED'] = 'Zamknięto';
+?>
